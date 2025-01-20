b2a184dda9cc22af989e3a01434acf1c99ebb367
goodness5 <goodnesskolapo@gmail.com>
2025-01-20 11:00:15 +0100

    shamir's secret sharing implemented

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod polynomials;
+mod shamirs_secret_sharing;
 
-use zk_implementations::polynomials::univalent::{self, PolynomialTrait};
+
+
+use zk_implementations::{polynomials::univalent::{self, PolynomialTrait}, shamirs_secret_sharing::core};
 
 // use crate::polynomials::univalent;
 
 fn main() {
     let polynomial = univalent::Polynomial::new(vec![(1, 2), (3, 18), (6, 6), (-1, 4)]);
-    // println!("{}", polynomial.represent());
-    // print!("{}", polynomial.evaluate(5));
-    print!("{}", polynomial.interpolate())
+    println!("{}", polynomial.represent());
+    print!("{}", polynomial.evaluate(5));
+    print!("{}", polynomial.interpolate());
+    core::secret_sharing_scheme();
 }
